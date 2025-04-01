5650316f34b6e4a42dfd7526a6617d805fc9e553
MzamoMahaeng8310m <mzamo.mahaeng@gmail.com>
2025-04-01 20:21:02 +0200

    the completed project with ui and api

--- GitPush.ps1
+++ GitPush.ps1
@@ -1,14 +1,6 @@
 Write-Host "Enter commit message:" -ForegroundColor Cyan
 $commitMsg = Read-Host
-
-# Stage all changes
 git add .
-
-# Show the current status
 git status
-
-# Commit with user-provided message
 git commit -m "$commitMsg"
-
-# Push to the main branch
 git push -u origin main
--- tests/ui/checkout.test.js
+++ tests/ui/checkout.test.js
@@ -11,33 +11,33 @@ test.describe.serial('UI Checkout Tests', () => {
     const inventoryPage = new InventoryPage(page);
     const cartPage = new CartPage(page);
 
-    // Login as standard user
+   
     const user = uiCredentials.validUsers.find(u => u.username === 'standard_user');
     await loginPage.goto();
     await loginPage.login(user.username, user.password);
 
-    // Confirm we're on inventory
+    
     await inventoryPage.expectOnInventoryPage();
 
-    // Add an item
+    
     await page.click('.inventory_item button:has-text("Add to cart")');
 
-    // Open cart
+    
     await cartPage.openCart();
   });
 
   test('Complete checkout flow', async ({ page }) => {
     const checkoutPage = new CheckoutPage(page);
 
-    // Start checkout
-    await page.click('[data-test="checkout"]');  // <--- updated
+    
+    await page.click('[data-test="checkout"]'); 
 
-    // Enter details
+   
     await checkoutPage.enterUserInfo(checkout.data.firstName, checkout.data.lastName,checkout.data.zipCode);
     await checkoutPage.continueToOverview();
     await checkoutPage.finishCheckout();
 
-    // Confirm success
+   
     await checkoutPage.expectCheckoutComplete();
   });
 });
