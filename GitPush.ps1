896d00a9a0399ea47e37094d98b7edeee2a3f945
MzamoMahaeng8310m <mzamo.mahaeng@gmail.com>
2025-09-01 13:47:59 +0200

    changed the commi file

--- GitPush.ps1
+++ GitPush.ps1
@@ -3,6 +3,6 @@ $commitMsg = Read-Host
 git add .
 git status
 git commit -m "$commitMsg"
-Write-Host "What is the branch you are comming to: " -ForegroundColor Cyan
-$brachName = Read-Host
+#Write-Host "What is the branch you are comming to: " -ForegroundColor Cyan
+#$brachName = Read-Host
 git push -u origin $brachName
