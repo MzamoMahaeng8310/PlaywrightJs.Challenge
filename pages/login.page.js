7aca4462c80afd2cb4f1de84a96897527498a4bf
MzamoMahaeng8310m <mzamo.mahaeng@gmail.com>
2026-05-10 16:27:37 +0200

    test

--- pages/login.page.js
+++ pages/login.page.js
@@ -5,7 +5,7 @@ export class LoginPage {
     constructor(page) {
 
         this.page = page;
-        this.usernameInput = '#user-name';
+        this.usernameInput = '#user-name'; 
         this.passwordInput = '#password';
         this.loginButton = '#login-button';
         this.errorMessage = '[data-test="error"]';
